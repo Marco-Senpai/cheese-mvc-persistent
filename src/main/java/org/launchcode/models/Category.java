28b985a1b2204167424ccdc99c6c9ff710c3ff66
Marco-Senpai <mcneilmarcus92@gmail.com>
2019-07-15 19:40:56 -0500

    part 1 and 2

--- src/main/java/org/launchcode/models/Category.java
+++ src/main/java/org/launchcode/models/Category.java
@@ -4,12 +4,19 @@ import org.springframework.data.annotation.Id;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
 public class Category {
+    @OneToMany
+    @JoinColumn(name = "category_id")
+    private List<Cheese> cheeses = new ArrayList<>();
 
     @Id
     @GeneratedValue
@@ -22,9 +29,25 @@ public class Category {
     public Category() {
 
     }
-
     public Category(String name) {
 
     }
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Cheese> getCheeses() {
+        return cheeses;
+
+    }
 }
+
 
---
+++ src/main/java/org/launchcode/models/Menu.java
@@ -0,0 +1,12 @@
+package org.launchcode.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Menu {
+    @Id
+    @GeneratedValue
+    public String name
+}
